95c2c3f908a8169b6f15a0b31c468af98c44dfbb
Gban-jb <jeebanbashyal1234@gmail.com>
2024-11-29 00:03:18 -0500

    hi

---
+++ Small_Projects/src/Palindrone.java
@@ -0,0 +1,12 @@
+//Jeeban Bashyal
+public class Palindrone {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
